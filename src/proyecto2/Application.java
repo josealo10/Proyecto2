2a638cd844ba8cb4386b5981c6417d6399ded7db
Alessandro Fazio <alessandro.fazio.perez@gmail.com>
2018-10-13 11:10:59 -0600

    Conexion a MySQL

--- src/proyecto2/Application.java
+++ src/proyecto2/Application.java
@@ -1,12 +1,14 @@
 package proyecto2;
 
+import proyecto2.data.RelDatabase;
+
 /*
  * @author Alessandro Fazio Perez / Jose Alonso Alfaro Perez
  */
 public class Application {
 
     public static void main(String[] args) {
-
+        RelDatabase r = new RelDatabase();
     }
 
 }
