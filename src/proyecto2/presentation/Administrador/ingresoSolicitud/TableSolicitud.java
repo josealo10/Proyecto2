82381f40bfef5d3ca6f0c732fbedf68e5ef54340
Alonso <josealonso.alfa@gmail.com>
2018-10-18 14:13:23 -0600

    Fecha

--- src/proyecto2/presentation/Administrador/ingresoSolicitud/TableSolicitud.java
+++ src/proyecto2/presentation/Administrador/ingresoSolicitud/TableSolicitud.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto2.presentation.Administrador.ingresoSolicitud;
 
 import javax.swing.table.AbstractTableModel;
